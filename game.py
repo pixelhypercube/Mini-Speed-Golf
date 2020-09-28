d29ec967a9a3e79a6429eddb988b5b207da92246
pixelhypercube <6thetce@gmail.com>
2020-09-28 17:31:14 +0800

    Editing levels

--- game.py
+++ game.py
@@ -12,8 +12,8 @@ class Color:
     white = (255,255,255)
     grey = (127,127,127)
     red = (255,40,0)
-    orange = (200,210,0)
-    yellow = (205,125,0)
+    yellow = (200,210,0)
+    orange = (185,105,0)
     green = (0,127,50)
     blue = (0,50,255)
     purple = (150,30,255)
@@ -85,7 +85,7 @@ class Hole:
         self.r = r
         self.color = color
     def show(self):
-        pg.draw.circle(frame,self.color,(self.x,self.y),self.r)
+        pg.draw.circle(frame,self.color,(int(self.x),int(self.y)),self.r)
     def update(self):
         self.x+=self.vx
         self.y+=self.vy
@@ -155,14 +155,14 @@ class Button:
 
 class GameScreen:
     def __init__(self):
-        self.playBtn = Button(WIDTH/2,HEIGHT/2,100,25,Color.blue,Color.yellow,Color.white,Color.grey,"Start Game!","levels")
-        self.backBtn = Button(100,HEIGHT/4,50,25,Color.blue,Color.yellow,Color.white,Color.grey,"Back","home")
+        self.playBtn = Button(WIDTH/2,HEIGHT/2,100,25,Color.blue,Color.orange,Color.white,Color.grey,"Start Game!","levels")
+        self.backBtn = Button(100,HEIGHT/4,50,25,Color.blue,Color.orange,Color.white,Color.grey,"Back","home")
         self.lvlNumBtns = []
         self.blocks = []
         for i in range(1,6):
-            self.lvlNumBtns.append(Button(i*100,(HEIGHT/2),30,30,Color.blue,Color.yellow,Color.white,Color.grey,str(i),"level"+str(i)))
+            self.lvlNumBtns.append(Button(i*100,(HEIGHT/2),30,30,Color.blue,Color.orange,Color.white,Color.grey,str(i),"level"+str(i)))
         for i in range(6,11):
-            self.lvlNumBtns.append(Button((i-5)*100,(HEIGHT/2)+100,30,30,Color.blue,Color.yellow,Color.white,Color.grey,str(i),"level"+str(i)))
+            self.lvlNumBtns.append(Button((i-5)*100,(HEIGHT/2)+100,30,30,Color.blue,Color.orange,Color.white,Color.grey,str(i),"level"+str(i)))
         self.player = Ball(50,HEIGHT/2,10,Color.white)
         self.hole = Hole(550,250,15,Color.black)
         self.strokes = 0
@@ -186,78 +186,136 @@ class GameScreen:
                 Block(200,100,75,75,Color.orange),
                 Block(300,400,75,75,Color.orange)
             ]
-            self.player = Ball(100,HEIGHT/2,10,Color.white)
+            self.player = Ball(100,HEIGHT/2,5,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
         elif num==2:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(0,HEIGHT-200,50,50,Color.orange),
+                Block(50,HEIGHT-250,50,50,Color.orange),
+                Block(100,HEIGHT-300,50,50,Color.orange),
+                Block(150,HEIGHT-350,50,50,Color.orange),
+                Block(200,HEIGHT-400,50,50,Color.orange),
+                Block(250,HEIGHT-450,50,50,Color.orange),
+                Block(300,HEIGHT-500,50,50,Color.orange),
+                Block(350,HEIGHT-550,50,50,Color.orange),
+                Block(400,HEIGHT-600,50,50,Color.orange),
+                # Block(450,HEIGHT-650,50,50,Color.orange),
+                
+                Block(0,HEIGHT-0,50,50,Color.orange),
+                Block(50,HEIGHT-50,50,50,Color.orange),
+                Block(100,HEIGHT-100,50,50,Color.orange),
+                Block(150,HEIGHT-150,50,50,Color.orange),
+                Block(200,HEIGHT-200,50,50,Color.orange),
+                Block(250,HEIGHT-250,50,50,Color.orange),
+                Block(300,HEIGHT-300,50,50,Color.orange),
+                Block(350,HEIGHT-350,50,50,Color.orange),
+                Block(400,HEIGHT-400,50,50,Color.orange),
+                Block(450,HEIGHT-450,50,50,Color.orange),
+                Block(500,HEIGHT-500,50,50,Color.orange),
+                Block(550,HEIGHT-550,50,50,Color.orange),
+                Block(600,HEIGHT-600,50,50,Color.orange),
             ]
-            self.player = Ball(50,HEIGHT/2,10,Color.white)
-            self.hole = Hole(550,250,15,Color.black)
-            self.par = 3
+            self.player = Ball(50,HEIGHT-75,5,Color.white)
+            self.hole = Hole(525,50,15,Color.black)
+            self.par = 4
         elif num==3:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(50,50,WIDTH-100,50,Color.orange),
+                Block(50,50,50,HEIGHT-100,Color.orange),
+                Block(WIDTH-100,50,50,HEIGHT-100,Color.orange),
+                Block(50,WIDTH-100,WIDTH-150,50,Color.orange),
+                Block(150,150,50,HEIGHT-200,Color.orange),
+                Block(150,150,WIDTH-300,50,Color.orange),
+                Block(WIDTH-200,150,50,HEIGHT-300,Color.orange),
+                Block(250,HEIGHT-200,200,50,Color.orange)
             ]
-            self.player = Ball(50,HEIGHT/2,10,Color.white)
-            self.hole = Hole(550,250,15,Color.black)
-            self.par = 3
+            self.player = Ball(125,HEIGHT-150,5,Color.white)
+            self.hole = Hole(WIDTH/2,HEIGHT/2,15,Color.black)
+            self.par = 6
         elif num==4:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(100,100,75,75,Color.orange),
+                Block(100,HEIGHT-200,75,75,Color.orange),
+                Block(WIDTH-200,100,75,75,Color.orange),
+                Block(WIDTH-200,HEIGHT-200,75,75,Color.orange),
             ]
-            self.player = Ball(50,HEIGHT/2,10,Color.white)
+            self.player = Ball(50,HEIGHT/2,5,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
         elif num==5:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(0,100,WIDTH,25,Color.orange),
+                Block(0,400,WIDTH,25,Color.orange),
+                Block(0,200,WIDTH-200,25,Color.orange),
+                Block(WIDTH-200,200,25,HEIGHT-475,Color.orange),
+                Block(0,300,WIDTH-200,25,Color.orange),
             ]
-            self.player = Ball(50,HEIGHT/2,10,Color.white)
-            self.hole = Hole(550,250,15,Color.black)
+            self.player = Ball(50,175,5,Color.white)
+            self.hole = Hole(50,375,15,Color.black)
             self.par = 3
         elif num==6:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(100,0,WIDTH-200,25,Color.orange),
+                Block(100,HEIGHT-25,WIDTH-200,25,Color.orange)
             ]
             self.player = Ball(50,HEIGHT/2,10,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
         elif num==7:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block(100,0,WIDTH-200,25,Color.orange),
+                Block(100,100,WIDTH-200,25,Color.orange),
+                Block(100,200,WIDTH-200,25,Color.orange),
+                Block(100,300,WIDTH-200,25,Color.orange),
+                Block(100,400,WIDTH-200,25,Color.orange),
+                Block(100,500,WIDTH-200,25,Color.orange),
+                Block(100,575,WIDTH-200,25,Color.orange),
             ]
-            self.player = Ball(50,HEIGHT/2,10,Color.white)
-            self.hole = Hole(550,250,15,Color.black)
+            self.player = Ball(50,HEIGHT/1.1,10,Color.white)
+            self.hole = Hole(550,50,15,Color.black)
             self.par = 3
         elif num==8:
             self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
+                Block( 439 , 185 , 63 , 80 ,Color.orange),
+                Block( 286 , 161 , 35 , 55 ,Color.orange),
+                Block( 144 , 275 , 70 , 65 ,Color.orange),
+                Block( 449 , 169 , 69 , 23 ,Color.orange),
+                Block( 152 , 166 , 53 , 73 ,Color.orange),
+                Block( 375 , 394 , 72 , 67 ,Color.orange),
+                Block( 134 , 201 , 54 , 39 ,Color.orange),
+                Block( 119 , 165 , 37 , 74 ,Color.orange),
+                Block( 204 , 327 , 60 , 22 ,Color.orange),
+                Block( 131 , 477 , 67 , 32 ,Color.orange),
+                Block( 331 , 104 , 69 , 78 ,Color.orange),
+                Block( 214 , 365 , 68 , 37 ,Color.orange),
+                Block( 279 , 111 , 78 , 42 ,Color.orange),
+                Block( 72 , 484 , 44 , 33 ,Color.orange),
+                Block( 76 , 427 , 49 , 46 ,Color.orange),
+                Block( 120 , 108 , 60 , 27 ,Color.orange),
+                Block( 265 , 182 , 42 , 32 ,Color.orange),
+                Block( 52 , 364 , 77 , 46 ,Color.orange),
+                Block( 384 , 199 , 71 , 47 ,Color.orange),
+                Block( 468 , 100 , 27 , 56 ,Color.orange),
             ]
             self.player = Ball(50,HEIGHT/2,10,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
         elif num==9:
-            self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
-            ]
+            # self.blocks = [
+            #     Block(200,100,75,75,Color.orange),
+            #     Block(300,400,75,75,Color.orange)
+            # ]
+            self.blocks = []
             self.player = Ball(50,HEIGHT/2,10,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
         elif num==10:
-            self.blocks = [
-                Block(200,100,75,75,Color.orange),
-                Block(300,400,75,75,Color.orange)
-            ]
+            # self.blocks = [
+            #     Block(200,100,75,75,Color.orange),
+            #     Block(300,400,75,75,Color.orange)
+            # ]
+            self.blocks = []
             self.player = Ball(50,HEIGHT/2,10,Color.white)
             self.hole = Hole(550,250,15,Color.black)
             self.par = 3
@@ -287,15 +345,43 @@ class GameScreen:
                 running = False
             elif event.type == pg.MOUSEBUTTONUP:
                 self.strokes+=1
-                distance = 0.05*math.sqrt((mouseX-self.player.x)**2+(mouseY-self.player.y)**2)
+                distance = 0.1*math.sqrt((mouseX-self.player.x)**2+(mouseY-self.player.y)**2)
                 angle = math.atan2(self.player.y-mouseY,self.player.x-mouseX)
                 self.player.vx = distance*math.cos(angle)
                 self.player.vy = distance*math.sin(angle)
     def showScored(self):
+        scoredStr = ""
+        score = self.strokes-self.par
+        if score==-3:
+            scoredStr="Albatross"
+        elif score==-2:
+            scoredStr="Eagle"
+        elif score==-1:
+            scoredStr="Birdie"
+        elif score==0:
+            scoredStr="Par"
+        elif score==1:
+            scoredStr="Bogey"
+        elif score==2:
+            scoredStr="Double Bogey"
+        elif score==3:
+            scoredStr="Triple Bogey"
+        else:
+            scoredStr=""
+            # scoredStr="+"+str(score) if score>0 else str(score) if score==0 else "±"+str(score)
         pg.draw.rect(frame,Color.blue,(0,0,WIDTH,HEIGHT))
-        renderText("SCORED!!!",WIDTH/2,HEIGHT/2,fontSize=40)
-        Button(WIDTH/2,HEIGHT/1.6,100,30,Color.orange,Color.yellow,Color.green,Color.white,"Next Level!","level"+str(self.currentLevel+1)).show()
-        Button(WIDTH/2,HEIGHT/1.3,100,30,Color.orange,Color.yellow,Color.green,Color.white,"Go back!","levels").show()
+        renderText("Finished!",WIDTH/2,HEIGHT/3,fontSize=40)
+        if (self.strokes==1):
+            renderText("Hole In One!",WIDTH/2,HEIGHT/2.5,fontSize=30)
+        renderText(scoredStr,WIDTH/2,HEIGHT/2.5,fontSize=30)
+        if score>0:
+            renderText("Score: "+"+"+str(score),WIDTH/2,HEIGHT/2,fontSize=40)
+        elif score==0:
+            renderText("Score: "+"±"+str(score),WIDTH/2,HEIGHT/2,fontSize=40)
+        else:
+            renderText("Score: "+str(score),WIDTH/2,HEIGHT/2,fontSize=40)
+        Button(WIDTH/2,HEIGHT/1.6,100,30,Color.orange,Color.green,Color.yellow,Color.white,"Next Level!","level"+str(self.currentLevel+1)).show()
+        Button(WIDTH/2,HEIGHT/1.3,100,30,Color.orange,Color.green,Color.yellow,Color.white,"Go back!","levels").show()
 
 gameScreen = GameScreen()
 
---
+++ generator.py
@@ -0,0 +1,4 @@
+import random as r
+
+for i in range(0,20):
+    print("Block(",str(r.randint(50,500)),",",str(r.randint(50,500)),",",str(r.randint(20,80)),",",str(r.randint(20,80)),",Color.orange),")
